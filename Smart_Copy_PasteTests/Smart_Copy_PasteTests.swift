3a2c7d82c12bc6df62f4395b84f884cbc3905502
V S S DHEERAJ KOTTE <kottedheeraj@gmail.com>
2020-06-08 16:07:45 +0530

    Initial Commit

---
+++ Smart_Copy_PasteTests/Smart_Copy_PasteTests.swift
@@ -0,0 +1,34 @@
+//
+//  Smart_Copy_PasteTests.swift
+//  Smart_Copy_PasteTests
+//
+//  Created by KOTTE V S S DHEERAJ on 08/06/20.
+//  Copyright © 2020 KOTTE V S S DHEERAJ. All rights reserved.
+//
+
+import XCTest
+@testable import Smart_Copy_Paste
+
+class Smart_Copy_PasteTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
